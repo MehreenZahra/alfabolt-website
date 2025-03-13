ab039ee4f60ea85d5b175d0144314e39d0c69b59
MehreenZahra <codewithMehreen1@gmail.com>
2025-03-13 17:49:07 +0500

    styling issues fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import Navbar from "@/components/navbar/index";
 import { CoreServices } from "@/components/core-services";
 
 export default function Home() {
   return (
-    <div >
-      <Navbar/>
-      <CoreServices/>
+    <div>
+      <Navbar />
+      <CoreServices />
     </div>
   );
 }
